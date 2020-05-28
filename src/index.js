6d04721d31e8a74d18a1fc6794a4c1b87693ba46
Devesh Avutu <devesh14may@gmail.com>
2020-05-28 18:34:39 +0530

    initialising project

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphQLHttp = require("express-graphql");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	}
+	next();
+});
+
+app.use(
+	"/graphql",
+	graphQLHttp({
+		schema,
+		rootValue: resolvers,
+		graphiql: true,
+	})
+);
+const MONGOOSE_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@groceries-cluster-omlgs.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+mongoose
+	.connect(MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		app.listen(3000, () => {
+			console.log("Server running");
+		});
+	})
+	.catch((err) => console.log(err));
